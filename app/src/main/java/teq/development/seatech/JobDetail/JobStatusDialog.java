d463b9da042eefe8fd65becdeb6ee6bf74f97038
teqmavens <tqminternal@gmail.com>
2018-06-08 11:14:55 +0530

    On 7thJune Evening2nd

--- app/src/main/java/teq/development/seatech/JobDetail/JobStatusDialog.java
+++ app/src/main/java/teq/development/seatech/JobDetail/JobStatusDialog.java
@@ -1,6 +1,7 @@
 package teq.development.seatech.JobDetail;
 
 import android.app.Dialog;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,8 +11,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.CompoundButton;
+import android.widget.PopupWindow;
 
 import teq.development.seatech.R;
+import teq.development.seatech.databinding.DialogJobstatusBinding;
 
 public class JobStatusDialog extends DialogFragment{
 
@@ -33,7 +37,20 @@ public class JobStatusDialog extends DialogFragment{
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
        View rootView = inflater.inflate(R.layout.dialog_jobstatus,container,false);
+        DialogJobstatusBinding binding = DataBindingUtil.bind(rootView);
+        initViews(binding);
        return rootView;
+    }
+
+    private void initViews(final DialogJobstatusBinding binding) {
+        binding.checkboxyes.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    displayPopupwindow(binding.checkboxyes);
+                }
+            }
+        });
     }
 
     @NonNull
@@ -52,8 +69,50 @@ public class JobStatusDialog extends DialogFragment{
         int height = dm.heightPixels;
         int width = dm.widthPixels;
         ViewGroup.LayoutParams params = getDialog().getWindow().getAttributes();
-        params.width = width-300;
+        params.width = width-400;
         params.height = height-400;
         getDialog().getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+    }
+
+    private void displayPopupwindow(View anchorview) {
+        PopupWindow popup = new PopupWindow(getActivity());
+        View layout = getLayoutInflater().inflate(R.layout.popupwd_jcyes, null);
+        popup.setContentView(layout);
+        /*DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int density = dm.densityDpi;
+        if (density == DisplayMetrics.DENSITY_HIGH) {
+            popup.setHeight(70);
+            popup.setWidth(200);
+        } else if (density == DisplayMetrics.DENSITY_MEDIUM) {
+
+            popup.setHeight(90);
+            popup.setWidth(250);
+            //  Toast.makeText(Menuu.this, "DENSITY_MEDIUM... Density is " + String.valueOf(density), Toast.LENGTH_LONG).show();
+        } else if (density == DisplayMetrics.DENSITY_LOW) {
+            popup.setHeight(60);
+            popup.setWidth(140);
+            //   Toast.makeText(Menuu.this, "DENSITY_LOW... Density is " + String.valueOf(density), Toast.LENGTH_LONG).show();
+        } else if (density == DisplayMetrics.DENSITY_XHIGH) {
+            popup.setHeight(100);
+            popup.setWidth(270);
+            //  Toast.makeText(Menuu.this, "DENSITY_XHIGH... Density is " + String.valueOf(density), Toast.LENGTH_LONG).show();
+        } else if (density == DisplayMetrics.DENSITY_XXHIGH) {
+            popup.setHeight(130);
+            popup.setWidth(320);
+            // Toast.makeText(Menuu.this, "DENSITY_XXHIGH... Density is " + String.valueOf(density), Toast.LENGTH_LONG).show();
+        } else if (density == DisplayMetrics.DENSITY_XXXHIGH) {
+            popup.setHeight(140);
+            popup.setWidth(370);
+            //  Toast.makeText(Menuu.this, "DENSITY_XXHIGH... Density is " + String.valueOf(density), Toast.LENGTH_LONG).show();
+        } else {
+        }*/
+       // RelativeLayout rl_company = (RelativeLayout) layout.findViewById(R.id.rl_company);
+
+
+
+        popup.setOutsideTouchable(true);
+        popup.setFocusable(true);
+        popup.showAsDropDown(anchorview);
     }
 }
